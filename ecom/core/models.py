09c215e13007c7c36d83c0bd9842457c523f662b
alikaali100 <alikaramouzian@icloud.com>
2024-12-02 16:30:35 +0330

    adding templates and fix some models

--- ecom/core/models.py
+++ ecom/core/models.py
@@ -5,8 +5,8 @@ from core.managers import LogicalDeleteManager
 class BaseModel(models.Model):
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
-    is_deleted = models.BooleanField(default=False)
-
+    is_deleted = models.BooleanField(default=False,blank=True,null=True)
+    deleted_at = models.DateTimeField(auto_now=True)
     class Meta:
         abstract = True
 
--- ecom/orders/models.py
+++ ecom/orders/models.py
@@ -19,11 +19,16 @@ class DiscountCode(BaseModel):
         return self.code
 
 class Order(BaseModel):
+    STATUS =(
+        ('pending','Pending'),
+        ('completed','Completed'),
+        ('canceled','Canceled')
+    )
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     discount_code = models.ForeignKey(DiscountCode, on_delete=models.SET_NULL, null=True, blank=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50,choices=STATUS)
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer}"
