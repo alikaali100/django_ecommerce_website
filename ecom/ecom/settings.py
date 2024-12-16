a7618265d57b4357a966fda3351e9c006bd67f9d
alikaali100 <alikaramouzian@icloud.com>
2024-12-16 16:31:42 +0330

    adding jwt

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -136,6 +136,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10, 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 #CORS SETTINGS
